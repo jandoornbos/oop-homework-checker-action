b1e7306846558fa1fd2a13c9c23e215736e5a478
Jan Doornbos <jan.doornbos@nhlstenden.com>
2023-11-27 15:50:20 +0100

    Start with checking files

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # OOP Homework Checker
 
 Script that checks if the assignments are handed in.
+
+## How to use
+
+The root folder of the repository should contain folders of all the weeks. The format is `week-xx`. An example repositories root folder might look like this.
+
+```
+week-1
+week-2
+week-3
+README.md
+.gitignore
+```
--- index.js
+++ index.js
@@ -1,10 +1,48 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-var fs = require('fs');
+var fs = require('fs')
+var path = require('path')
 
 try {
-    console.log(fs.readdirSync('./'));
+    const fileList = fs.readdirSync('./')
+    let weeks = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    weeks.forEach(weekNumber => {
+        const weekDir = 'week-' + weekNumber
+        
+        if (fs.existsSync(weekDir)) {
+            // There is a folder with week-x
+            const contents = fs.readdirSync(weekDir)
+
+            const subDirs = contents.filter(item => fs.statSync(path.join(weekDir, item)).isDirectory())
+            const allFiles = []
+
+            subDirs.forEach(subDir => {
+                const subDirPath = path.join(weekDir, subDir)
+                const files = fs.readdirSync(subDirPath)
+
+                const javaFiles = files.filter(file => path.extname(file) === '.java')
+                allFiles.push(javaFiles)
+            })
+
+            checkFilesForWeek(weekNumber, allFiles)
+        }
+    });
 } catch (error) {
     core.setFailed(error.message)
+}
+
+function checkFilesForWeek(week, files) {
+    let possibleFiles = []
+    let expectedFiles = 0
+
+    if (week === 1) {
+        possibleFiles = [ 'Book.java' ]
+        expectedFiles = 1
+    }
+
+    if (files.length < expectedFiles) {
+        core.warning('Week ' + week + ' has not the expected amount of files!')
+    }
 }
