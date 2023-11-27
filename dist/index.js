50062f0028e41d296d31b7e1e27fcee20e8bcfcb
Jan Doornbos <jan.doornbos@nhlstenden.com>
2023-11-27 15:56:56 +0100

    Add compiled file

--- dist/index.js
+++ dist/index.js
@@ -30756,15 +30756,53 @@ module.exports = parseParams
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const core = __nccwpck_require__(5127);
-const github = __nccwpck_require__(3134);
+const core = __nccwpck_require__(5127)
+const github = __nccwpck_require__(3134)
 
-var fs = __nccwpck_require__(7147);
+var fs = __nccwpck_require__(7147)
+var path = __nccwpck_require__(1017)
 
 try {
-    console.log(fs.readdirSync('./'));
+    const fileList = fs.readdirSync('./')
+    let weeks = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    weeks.forEach(weekNumber => {
+        const weekDir = 'week-' + weekNumber
+        
+        if (fs.existsSync(weekDir)) {
+            // There is a folder with week-x
+            const contents = fs.readdirSync(weekDir)
+
+            const subDirs = contents.filter(item => fs.statSync(path.join(weekDir, item)).isDirectory())
+            const allFiles = []
+
+            subDirs.forEach(subDir => {
+                const subDirPath = path.join(weekDir, subDir)
+                const files = fs.readdirSync(subDirPath)
+
+                const javaFiles = files.filter(file => path.extname(file) === '.java')
+                allFiles.push(javaFiles)
+            })
+
+            checkFilesForWeek(weekNumber, allFiles)
+        }
+    });
 } catch (error) {
     core.setFailed(error.message)
+}
+
+function checkFilesForWeek(week, files) {
+    let possibleFiles = []
+    let expectedFiles = 0
+
+    if (week === 1) {
+        possibleFiles = [ 'Book.java' ]
+        expectedFiles = 1
+    }
+
+    if (files.length < expectedFiles) {
+        core.warning('Week ' + week + ' has not the expected amount of files!')
+    }
 }
 })();
 
