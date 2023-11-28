5fa73335f297868ba7e19163136531d514253d9c
Jan Doornbos <jan.doornbos@nhlstenden.com>
2023-11-28 12:11:24 +0100

    Add getters & setters check

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const github = require('@actions/github')
 
 var fs = require('fs')
 var path = require('path')
+var parser = require('java-parser')
 
 try {
     const fileList = fs.readdirSync('./')
@@ -45,4 +46,63 @@ function checkFilesForWeek(week, files) {
     if (files.length < expectedFiles) {
         core.warning('Week ' + week + ' has not the expected amount of files!')
     }
+
+    files.forEach(file => {
+        validateJavaFile(file);
+    })
+}
+
+function validateJavaFile(filePath, fieldName) {
+    const javaCode = fs.readFileSync(filePath, 'utf-8');
+
+    parser.parseCode(javaCode, { comments: false }, (err, parsedCode) => {
+        if (err) {
+            console.error('Error parsing Java code:', err);
+            return;
+        }
+
+        traverseAST(parsedCode, fieldName);
+    });
+}
+  
+function traverseAST(node) {
+    if (node.type === 'ClassDeclaration') {
+        validateFields(node);
+    }
+  
+    if (node.body && node.body.length) {
+        node.body.forEach((childNode) => {
+            traverseAST(childNode);
+        });
+    }
+}
+  
+function validateFields(clazz) {
+    const fields = clazz.body.filter(
+        (element) => element.type === 'FieldDeclaration'
+    );
+
+    fields.forEach((field) => {
+        const fieldName = field.variables[0].name;
+        const getterName = 'get' + fieldName[0].toUpperCase() + fieldName.slice(1);
+        const setterName = 'set' + fieldName[0].toUpperCase() + fieldName.slice(1);
+
+        const hasGetter = clazz.body.some(
+        (method) =>
+            method.type === 'MethodDeclaration' && method.name === getterName
+        );
+
+        const hasSetter = clazz.body.some(
+        (method) =>
+            method.type === 'MethodDeclaration' && method.name === setterName
+        );
+
+        if (!hasGetter || !hasSetter) {
+            core.error(
+                `Validation failed for field ${fieldName} in class ${clazz.name}. Missing ${
+                hasGetter ? 'setter' : 'getter'
+                }.`
+            );
+        }
+    });
 }
